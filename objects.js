deaa925a228e7ec5e064512ea4a95901ddcaa0b8
Jason Shockness <jshock12@gmail.com>
2019-06-18 04:23:45 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,7 +9,7 @@ function updatePlaylist(playlist, artist, song) {
 }
 
 function removeFromPlaylist(playlist, artist) {
-  delete playlist[artist]
+  delete playlist['Phil Ochs']
 
   return playlist
 }
